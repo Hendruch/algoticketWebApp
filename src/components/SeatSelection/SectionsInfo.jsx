51195945986862b897de4ba23be333b62d9bf88a
EduardoVarela0144 <014426634@ulsaoaxaca.edu.mx>
2023-05-02 16:38:34 -0600

    "Seat image"

--- src/components/SeatSelection/SectionsInfo.jsx
+++ src/components/SeatSelection/SectionsInfo.jsx
@@ -1,12 +1,12 @@
 import { SectionRowInfo } from "./SectionRowInfo"
 
-function SectionsInfo(){
-    return(
-        <div style={{width:'80%'}}className="text-xl font-bold space-y-4  py-10">
-            <SectionRowInfo  color="rgb(234 179 8)" section="A" />
-            <SectionRowInfo color="rgb(59 130 246)" section="B" />
-            <SectionRowInfo color="rgb(239 68 68)" section="C" />
-            <SectionRowInfo color="rgb(139 92 246)" section="D" />
+function SectionsInfo({ handleSeatsBySection }) {
+    return (
+        <div style={{ width: '80%' }} className="text-xl font-bold space-y-4  py-10">
+            <SectionRowInfo color="rgb(234 179 8)" section="A" handleSeatsBySection={handleSeatsBySection} />
+            <SectionRowInfo color="rgb(59 130 246)" section="B" handleSeatsBySection={handleSeatsBySection} />
+            <SectionRowInfo color="rgb(239 68 68)" section="C" handleSeatsBySection={handleSeatsBySection} />
+            <SectionRowInfo color="rgb(139 92 246)" section="D" handleSeatsBySection={handleSeatsBySection} />
         </div>
     )
 }
