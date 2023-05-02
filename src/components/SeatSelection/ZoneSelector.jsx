1a58817b10c6ce3b11fdec070704b7378423152c
EduardoVarela0144 <014426634@ulsaoaxaca.edu.mx>
2023-05-02 16:48:04 -0600

    "changue to foto"

--- src/components/SeatSelection/ZoneSelector.jsx
+++ src/components/SeatSelection/ZoneSelector.jsx
@@ -1,12 +1,10 @@
-
+import Escenario from "../../assets/img/Escenario.png"
 
 function ZoneSelector() {
 
     return (
-        <div className="bg-black w-full h-full pt-5 px-5">
-            <div className="grid grid-cols-7 text-center ">
-                <div style={{ color: 'white' }} className="col-span-7 ..." ><p>Escenario</p></div>
-            </div>
+        <div className="bg-black w-full h-full mb-10 px-5">
+            <img src={Escenario} className="h-auto w-auto" />
         </div>
 
     )
