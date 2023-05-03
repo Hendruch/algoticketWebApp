26b6579678600150a03b99066c33945b057d5a33
Luis Morales <luismc2211@gmail.com>
2023-05-03 16:43:05 -0600

    change

--- src/components/SeatSelection/EventInfo.jsx
+++ src/components/SeatSelection/EventInfo.jsx
@@ -1,13 +1,13 @@
 function EventInfo (){
     return(
-        <div className="space-y-4 p-10 text-xl font-bold" >
+        <div className="space-y-4 p-10 text-xl" >
             <p>Límite de edad: A partir de 1 año</p>
             <p>Paga boleto a partir de : A partir de 4 años</p>
             <p>Restricciones : No se aceptan armas ni equipo fotográfico</p>
             <p>Límite de acceso : 12000 personas</p>
             <p>Servicios : Alimentos, Bebidas, Sanitarios</p>
             <p>Duración aproximada : 3 horas</p>
-            <p>Página oficial : <a href="https://luismigueloficial.com/">https://luismigueloficial.com/</a></p>
+            <p>Página oficial : <a className="underline underline-offset-1" href="https://luismigueloficial.com/">https://luismigueloficial.com/</a></p>
             <p>Personas con discapacidad: Capacidad de 50 personas</p>
             <p>Event Info : Vive la experiencia de Luis Miguel en concierto</p>
         </div>
