8ab344a6df6971ac04ef4033c4167197d1b5bb8e
Hendruch <hendruchrasgado@gmail.com>
2023-05-02 18:40:51 -0600

    Login y registro

--- src/router/PageRoutes.jsx
+++ src/router/PageRoutes.jsx
@@ -2,6 +2,8 @@ import { Routes, Route,  BrowserRouter } from "react-router-dom";
 import Home from "../pages/Home";
 import EventsDashboard from "../pages/EventsDashboard";
 import SeatSelection from "../pages/SeatSelection"
+import LoginPage from "../pages/Login";
+import RegistroPage from "../pages/Registro";
 
 function PageRoutes() {
     return (
@@ -10,7 +12,9 @@ function PageRoutes() {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/CRUDe" element={<EventsDashboard />} />
-                <Route path="/asientos" element={<SeatSelection />} /> 
+                <Route path="/asientos" element={<SeatSelection />} />
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/registro" element={<RegistroPage />} />
             </Routes>
         </BrowserRouter>
       </>
