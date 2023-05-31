33b3a99db56dcb70d05462dd29441eea94c3bab3
Eduardo Varela <014426634@ulsaoaxaca.edu.mx>
2023-05-30 20:04:17 -0600

    "Seat api selector"

--- src/repositories/EventsRepository.jsx
+++ src/repositories/EventsRepository.jsx
@@ -59,7 +59,7 @@ class EventsRepository {
 
   async getSections(seccion, eventoId){
 
-    const referencia = doc(db,'evento', eventoId);
+    const referencia = doc(db, 'evento', eventoId);
     const sectionsCollectionRef = collection(db, 'seccion');
     const querySnapshot = await getDocs(query(
       sectionsCollectionRef,
@@ -68,12 +68,31 @@ class EventsRepository {
     ));
   
     const sections = [];
-
-    querySnapshot.forEach((doc) => {
-      sections.push({ id: doc.id, ...doc.data() });
-    });
-
+  
+    await Promise.all(querySnapshot.docs.map(async (doc) => {
+      const sectionData = doc.data();
+      const sectionWithReferences = {
+        id: doc.id,
+        ...sectionData,
+        rango_asientos: [], // Array to hold objects referenced in 'rango_asientos'
+      };
+  
+      const references = sectionData.rango_asientos || [];
+  
+      const referencePromises = references.map((reference) => getDoc(reference));
+      const referenceSnapshots = await Promise.all(referencePromises);
+  
+      referenceSnapshots.forEach((referenceSnapshot) => {
+        if (referenceSnapshot.exists()) {
+          sectionWithReferences.rango_asientos.push(referenceSnapshot.data());
+        }
+      });
+  
+      sections.push(sectionWithReferences);
+    }));
+  
     return sections;
+  
 
   }
 }
